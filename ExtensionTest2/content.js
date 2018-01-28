6d00ef7ebaf8cb23736cf44ff97558bcc10b441a
Thomas Gauvin <thomasgauvin.cu@gmail.com>
2018-01-28 01:02:22 -0500

    Text summary popup working

--- ExtensionTest2/content.js
+++ ExtensionTest2/content.js
@@ -4,9 +4,9 @@
 //alert("Hello there.")
 
 /* Example of replacing text
-var s="<div>hello world <br/><li>First LI</li><li>Second LI</li></div>";   
-s = s.replace(/<li>/g, "#");  
-s = s.replace(/<\/li>/g,"<br/>"); 
+var s="<div>hello world <br/><li>First LI</li><li>Second LI</li></div>";
+s = s.replace(/<li>/g, "#");
+s = s.replace(/<\/li>/g,"<br/>");
 alert(s);
 */
 //var page = document.body
@@ -19,32 +19,62 @@ s.src = chrome.extension.getURL('content.js');
 document.head.appendChild(s);
 */
 
+
 //Adds our popup text to the html file
+var delayInMilliseconds = 700; //1 second
+
 var block_to_insert = document.createElement('div');
 
 $(block_to_insert).attr("id", "popup");
-$(block_to_insert).attr("style","background-color:#FFFFFF;position:absolute;width: 300px;visibility: hidden;")
-
+$(block_to_insert).attr("style","background-color:#FFFFFF;border-style:double;position:absolute;width: 330px; visibility: hidden;");
+$(paragraph).attr("style", "padding-top: 50px;padding-right: 10px;padding-bottom: 50px;padding-left: 10px;");
 var paragraph = document.createElement('p');
 paragraph.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
 block_to_insert.appendChild(paragraph);
 document.body.appendChild(block_to_insert);
 
-/*
-<div id="popup" style="background-color:#0000FF;
-        position:absolute;
-        width: 300px;
-        max-height: 250px;
-        visibility: hidden;">
-<p>
-</p>
-</div>"
-*/
+
+$("a").hover(function (event) {
+    var destination = $(this).attr("href");
+    document.getElementById("popup").innerHTML = destination;
+    //httpDestination = destination.substring(0,3) + destination.substring(4);
+
+    $.ajax({
+        url: destination, success: function (data) {
+              var content = document.createElement('span');
+              content.innerHTML = data;
+
+              var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://api.meaningcloud.com/summarization-1.0",
+                    "method": "POST",
+                    "headers": {
+                      "content-type": "application/x-www-form-urlencoded"
+                    },
+                    "data": {
+                      "key": "3413bd9e45d666fd46aa5e8f17e8e180",
+                      "txt": content.innerHTML,
+                      "url": "",
+                      "doc": "",
+                      "sentences": "2"
+                    }
+                  }
+
+              $.ajax(settings).done(function (response) {
+                document.getElementById("popup").innerHTML=response.summary;
+              })
+          ;}})
+
+
+})
 
 $("#popup").hover(function (event) {
     document.getElementById("popup").style.visibility = "visible";
 }
-, function () {
+,
+
+function () {
     document.getElementById("popup").style.visibility = "hidden";
 });
 
@@ -55,10 +85,14 @@ $("a").hover(function (event) {
     document.getElementById("popup").innerHTML = destination;
     $.ajax({
         url: destination, success: function (data) {
-            var x = event.pageX, y = event.pageY + 10;
+            var x = event.pageX-350, y = event.pageY +10;
             //Data contains the html file of the hovered webpage
             //TODO: Grab only the content text from the html document
             //so that it can be passed to the summarizer
+            //var content;
+            //content = data.innerText;
+            //console.log(content);
+
 
             //TODO: Set the inner html to the output of the summarizer (use
             //callback functions
@@ -70,11 +104,7 @@ $("a").hover(function (event) {
     });
 
 }
-,function() {
+
+/*,function() {
     document.getElementById("popup").style.visibility = "hidden";
-});
-
-
-
-
-
+}*/);
